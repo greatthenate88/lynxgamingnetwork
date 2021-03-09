3b9799cf6c458d0210c4c2b6cf694e560a4f33a3
malarcherc <60486526+malarcherc@users.noreply.github.com>
2021-03-09 13:33:04 -0500

    First page route
    test testr

--- src/Config/routes.js
+++ src/Config/routes.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import Home from '../pages/Home'
+import SignIn from '../pages/SignIn';
 
 
 
 
 const routes = [
+
+	{
+		path: '/SignIn',
+		component: SignIn,
+		isPrivate: false,
+	},
 	{
 		path: '/',
 		component: Home,
 		isPrivate: false,
 	},
 	{
-		path: '/',
+		path : '/',
 		component: Home,
 		isPrivate: true,
 	},
+	
+	
+
 ];
 
 export default routes;
